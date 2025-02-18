2e5f3d9ec05e7eaf6390769af73a7e53285b0d26
apetrenko <apetrenko@gseis.ru>
2025-02-18 13:59:25 +0500

    Задание 2. Шардирование.
    Реализовал первый вариант схемы с двумя шардами.

---
+++ mongo-sharding/scripts/mongo-init.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+###
+# Подключаемся к серверу конфигурации, чтобы проинициализировать
+###
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+{
+  _id : "config_server",
+     configsvr: true,
+  members: [
+    { _id : 0, host : "configSrv:27017" }
+  ]
+}
+);
+exit();
+EOF
+
+###
+# Инициализируем шарды
+###
+docker compose exec -T shard1-1 mongosh --port 27018 <<EOF
+rs.initiate(
+  {
+    _id : "shard1-1",
+    members: [
+      { _id : 0, host : "shard1-1:27018" }
+    ]
+  }
+);
+exit();
+EOF
+
+docker compose exec -T shard1-2 mongosh --port 27019 <<EOF
+rs.initiate(
+  {
+    _id : "shard1-2",
+    members: [
+      { _id : 1, host : "shard1-2:27019" }
+    ]
+  }
+);
+exit();
+EOF
+
+###
+# Инцициализация роутера
+###
+docker compose exec -T mongo_router mongosh --port 27020 <<EOF
+sh.addShard( "shard1-1/shard1-1:27018");
+sh.addShard( "shard1-2/shard1-2:27019");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+exit();
+EOF
+
+###
+# Инициализируем бд
+###
+docker compose exec -T mongo_router mongosh --port 27020 <<EOF
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+exit();
+EOF
+
+###
+# Выводим общее количество документов В БД somedb
+###
+docker compose exec -T mongo_router mongosh --port 27020 <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+###
+# Выводим количество документов В БД somedb инстанса shard1-1
+###
+docker compose exec -T shard1-1 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+###
+# Выводим количество документов В БД somedb инстанса shard1-2
+###
+docker compose exec -T shard1-2 mongosh --port 27019 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
